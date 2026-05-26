aab918724f8f19f1aa6fa7bd8f74b9e1e1710221
gabriel bessani ferreira <gabriel_bessani@hotmail.com.br>
2026-05-26 12:24:44 -0300

    navi correction

--- components/layout/bottom-nav.tsx
+++ components/layout/bottom-nav.tsx
@@ -41,7 +41,7 @@ export function BottomNav() {
   const [isMobile, setIsMobile] = useState(false)
 
   useEffect(() => {
-    const check = () => setIsMobile(window.innerWidth < 1024)
+    const check = () => setIsMobile(window.innerWidth < 768)
     check()
     window.addEventListener("resize", check)
     return () => window.removeEventListener("resize", check)
