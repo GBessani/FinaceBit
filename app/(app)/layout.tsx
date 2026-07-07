85f9c523a461b3b9f211f510e8742d8e91fe2d64
gabriel bessani ferreira <gabriel.personal.game@gmail.com>
2026-07-07 15:00:47 -0300

    fix: sidebar fixa na viewport no desktop, nao rola com o conteudo

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -18,7 +18,7 @@ export default async function AppLayout({ children }: { children: React.ReactNod
           <ConsultantBanner />
           <div className="flex flex-1">
             <Sidebar />
-            <main className="flex-1 min-w-0 p-4 lg:p-8 pt-16 lg:pt-8 pb-8 overflow-x-hidden">
+            <main className="flex-1 min-w-0 lg:ml-64 p-4 lg:p-8 pt-16 lg:pt-8 pb-8 overflow-x-hidden">
               <div className="max-w-6xl mx-auto">{children}</div>
             </main>
           </div>
--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -65,7 +65,7 @@ export default function Sidebar() {
       )}
 
       <aside
-        className={`fixed lg:sticky lg:top-0 lg:h-screen inset-y-0 left-0 z-50 w-64 bg-sidebar border-r border-sidebar-border transform transition-transform lg:transform-none ${
+        className={`fixed lg:top-0 lg:h-screen inset-y-0 left-0 z-50 w-64 bg-sidebar border-r border-sidebar-border transform transition-transform lg:transform-none ${
           isOpen ? "translate-x-0" : "-translate-x-full lg:translate-x-0"
         }`}
       >
